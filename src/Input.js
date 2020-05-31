092d1723ec64f77a4707d90c51f32e1da2be9404
sahal1 <sahal-1417@hotmail.com>
2020-05-31 05:34:17 +0300

    Complete input component render tests for success = false

--- src/Input.js
+++ src/Input.js
@@ -3,16 +3,33 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
 	render(){
+
+		const contents = this.props.success ? null : ( 
+
+				<form className="form-inline">
+					<input className="mb-2 mx-sm-3"
+						data-test="input-box"
+						type="text"
+						placeholder="enter guess" />
+
+					<button className="btn btn-primary mb-2"
+						data-test="submit-button"
+						type="submit">	
+						Submit
+					</button>	
+				</form>
+			)
+
 		return(
-			<div>
-				<button />
+			<div data-test="component-input">
+				{ contents }
 			</div>
 		) 
 	}
 }
 
-const mapStateToProps = (state) => {
-	return {};
+const mapStateToProps = ({success}) => {
+	return { success };
 }
  	 	 	
 export default connect(mapStateToProps)(Input); 	
--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -2,19 +2,19 @@ import { actionTypes } from '../actions';
 
 
 // way of mine
-export default (state=false, action) => {
+/*export default (state=false, action) => {
 	if(action.type == actionTypes.CORRECT_GUESS )
 		return true;
 	else
 		return false;
 }
-
-// way of course 
-/*export default (state=false, action) => {
+*/
+// way of course (prefered)
+export default (state=false, action) => {
 	switch(action.type){
 		case (actionTypes.CORRECT_GUESS):
 			return true;
 		default:
 			return false;
 	}
-}*/
+}
