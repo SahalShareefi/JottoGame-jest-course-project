96bacc62dce6e25422a9e10564743b242c751dae
sahal1 <sahal-1417@hotmail.com>
2020-06-30 10:43:51 +0300

    Install Redux Thunk Middleware

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import rootReducer from '../src/reducers'
+import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 export const findByTestAttr = (wrapper, val) => {
 	return wrapper.find(`[data-test="${val}"]`);
@@ -12,7 +13,8 @@ export const checkProps = (component, conformingProps) => {
 	const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name); //the argument of the function (Congrats Component propTypes 'existed there', the props that will be supplied e.g. "{ name: 'Julia' }", we r testing the properties like this: 'prop', Congrats Component name);
 	expect(propError).toBeUndefined();
 }
-
+ 
 export const storeFactory = (initialState) => {  // we want to create store for testing that matchs the configuration of the atual store
-	return createStore(rootReducer, initialState);
+	const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+	return createStoreWithMiddleware(rootReducer, initialState);
 }
